37245924d07f8016c35fd0cfe34cf6729ee90ba3
Alex Patterson <69934013+prontotheant@users.noreply.github.com>
2023-01-23 12:55:11 -0500

    Created file, added comments

---
+++ app.py
@@ -0,0 +1,19 @@
+#
+# File: app.py
+# Project: Healthcare Investment Tool
+# Author(s): Alex Patterson,
+# Purpose: contain page navigation and session information
+#
+
+# first time, in terminal: pip install flask
+# to run website, in terminal (windows):  flask --app app --debug run
+# flask is sensitive to file structure and names
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
