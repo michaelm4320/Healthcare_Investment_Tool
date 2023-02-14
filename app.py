e1925d21b7f21f3d176f642a0945732688faeb6f
Alex Patterson <69934013+prontotheant@users.noreply.github.com>
2023-02-14 11:03:19 -0500

    added some flask and jinja stuff

--- app.py
+++ app.py
@@ -6,14 +6,25 @@
 #
 
 # first time, in terminal: pip install flask
-# to run website, in terminal (windows and pycharm):  flask --app app --debug run
+# to run website, in terminal (windows):  flask --app app --debug run
 # flask is sensitive to file structure and names
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect, session
 
 app = Flask(__name__)
 
+test_variable1 = "Healthcare Investment Tool"
+test_variable2 = 6725432
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html', Radiant=test_variable1, Awesome=test_variable2)
+
+
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    if request.method == "GET":
+        return render_template('dashboard.html')
+
