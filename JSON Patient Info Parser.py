21edfa86c17cb0d13853d18d39fd81dadb29e0e9
kayloleon <69911814+kayloleon@users.noreply.github.com>
2023-04-24 22:11:08 -0400

    JSON Patient Infor Parser
    Looks for patient id, DOB, first, last name, and gender.

---
+++ JSON Patient Info Parser.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+
+# Function to recursively extract key values from nested JSON objects
+def extract_key(json_obj, key):
+    result = []
+    if isinstance(json_obj, dict):
+        for k, v in json_obj.items():
+            if k == key:
+                result.append(v)
+            else:
+                result.extend(extract_key(v, key))
+    elif isinstance(json_obj, list):
+        for item in json_obj:
+            result.extend(extract_key(item, key))
+    return result
+
+# local directory
+directory = "C:\\Synthea2\\output\\EarInfectionFiles"
+# Iterate through all files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):
+        # Open the JSON file
+        with open(os.path.join(directory, filename), encoding="utf8") as f:
+            nested_data = json.load(f)
+
+            # find patient id
+            find_id = extract_key(nested_data, 'id')[0]
+            print(f'Patient id: {find_id}')
+
+            # find patient Date of Birth
+            find_DOB = extract_key(nested_data, 'birthDate')[0]
+            print(f'Patient DOB: {find_DOB}')
+
+            # find patient First Name
+            find_first_name = extract_key(nested_data, 'given')[0][0]
+            print(f'Patient First Name: {find_first_name}')
+
+            # find patient Last Name
+            find_last_name = extract_key(nested_data, 'family')[0]
+            print(f'Patient Last Name: {find_last_name}')
+
+            # find patient Gender
+            find_gender = extract_key(nested_data, 'gender')[0]
+            print(f'Patient Gender: {find_gender}')
